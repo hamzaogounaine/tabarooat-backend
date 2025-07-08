1fd23288c400605b752cbed041d8475a71ec1da0
hamzaogounaine <hamz2487@gmail.com>
2025-07-08 22:23:37 +0100

    First commit

--- middlewares/loginMiddleware.js
+++ middlewares/loginMiddleware.js
@@ -1,6 +1,7 @@
 const Redis = require('ioredis')
 
-const redis = new Redis();
+const redis = new Redis(process.env.REDIS_URL || 'redis://127.0.0.1:6379');
+
 
 const limitLoginMiddleware = async (req , res , next) => {
     const max_attemps = 5;
@@ -10,7 +11,6 @@ const limitLoginMiddleware = async (req , res , next) => {
 
     const attempts = await redis.get(key);
 
-    console.log(attempts)
 
     if(attempts && parseInt(attempts) >= max_attemps) {
         return res.status(429).json({message : 'محاولات تسجيل دخول فاشلة متعددة من نفس الجهاز  المرجو المحاولة مرة اخرى بعد 5 دقاءق'})
