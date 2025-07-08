5a87474ad7819b64777a6a4732872681a070c237
hamzaogounaine <hamz2487@gmail.com>
2025-07-08 22:29:49 +0100

    First commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,7 @@ const crypto = require("crypto");
 const { serialize } = require("cookie");
 const Redis = require('ioredis')
 
-const redis = new Redis(process.env.REDIS_URL || 'redis://127.0.0.1:6379');
+const redis = new Redis(process.env.REDISCLOUD_URL || 'redis://127.0.0.1:6379');
 
 
 const transporter = nodemailer.createTransport({
